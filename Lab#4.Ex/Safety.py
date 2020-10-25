c718e7fdfff630dad0e8686da80eafde820a5d7f
Lord <123412342@inbox.ru>
2020-10-25 22:12:14 +0400

    Upd

--- Lab#4.Ex/Safety.py
+++ Lab#4.Ex/Safety.py
@@ -2,22 +2,18 @@ import re
 
 number = r"\d+"
 notNumber = r"\D+"
-f = open("Unsafe.txt", "r")
+f = open("Unsafe.txt", "r", encoding='utf-8')
 s = f.read()
 f.close()
-f = open("Safe.txt", "w")
+f = open("Safe.txt", "w", encoding='utf-8')
 found = re.findall(number, s)
 notFound = re.findall(notNumber, s)
 for i in range (len(found)):
     found[i] = int(found[i])**2*3+5
-print (len(found), len(notFound))
 if len(found) < len(notFound):
     found.append("")
 elif len(notFound) < len(found):
     notFound.append("")
-print (notFound)
-print (found)
-print(s[0])
 for i in range(len(found)):
     try:
         if int(s[0]):
--- Lab#4.Ex/TwoComas.py
+++ Lab#4.Ex/TwoComas.py
@@ -1,11 +1,11 @@
 import re
 
-twoComasSentence = r"[A-Z0-9][^,?!.]*,[^,?!.]*,[^?!.]*[.?!]"
+twoComasSentence = r"[A-Z0-9А-Я][^,?!.]*,[^,?!.]*,[^.]*?[\"]?[!?.]{1,3}"
 f = open("Macbeth.txt", "r")
 s = f.read()
 f.close
 f = open("MacOut.txt", "w")
 found = re.findall(twoComasSentence, s)
-for i in range(1, len(found)):
-    f.write(str(i) + ": " + str(found[i]) + "\n\n")
+for i in range(len(found)):
+    f.write(str(i+1) + ": " + str(found[i]) + "\n\n")
 f.close()
