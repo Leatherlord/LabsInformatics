cd9b0e6f219d9d4f145828ad6c1cb0eae0ce1b70
Leatherlord <123412342@inbox.ru>
2021-02-15 03:19:23 +0300

    code review fix

--- Lab#4.Ex/TwoComas.py
+++ Lab#4.Ex/TwoComas.py
@@ -3,7 +3,6 @@ import re
 twoComasSentence = r"[A-Z0-9А-Я][^,?!.]*,[^,?!.]*,[^.]*?[\"]?[!?.]{1,3}"
 f = open("Macbeth.txt", "r")
 s = f.read()
-f.close
 f = open("MacOut.txt", "w")
 found = re.findall(twoComasSentence, s)
 for i in enumerate(found):
